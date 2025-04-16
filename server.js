8e899dcd4ff078c33d73588538e833d7334ae480
AuoPuls <nedas.r52@gmail.com>
2025-04-16 20:25:22 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require('express');const path = require('path');const app = express();app.use(express.static('public'));app.set('view engine', 'ejs');app.set('views', path.join(__dirname, 'views'));app.get('/', (req, res) => res.render('index'));app.get('/paslaugos', (req, res) => res.render('services'));app.listen(3000);
